a1ff10f175fc90b0c7d088ac4727103629c3e931
badqiu <badqiu@gmail.com>
2015-10-15 16:23:04 +0800

    fixed Jetty tld error by classloader

--- web-admin/src/test/java/com/company/project/tools/WebAdminJettyServer.java
+++ web-admin/src/test/java/com/company/project/tools/WebAdminJettyServer.java
@@ -1,6 +1,7 @@
 package com.company.project.tools;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.eclipse.jetty.webapp.WebAppContext;
 /**
  * 开发调试使用的 Jetty Server
@@ -10,18 +11,25 @@ import org.eclipse.jetty.webapp.WebAppContext;
 public class WebAdminJettyServer {
 	
 	public static void main(String[] args) throws Exception {
-		Server server = buildNormalServer(7070, "/");
+		Server server = createServerInSource(7070, "/");
 		server.start();
 	}
 	
 	/**
-	 * 创建用于正常运行调试的Jetty Server, 以src/main/webapp为Web应用目录.
+	 * 创建用于开发运行调试的Jetty Server, 以src/main/webapp为Web应用目录.
+	 * @throws Exception 
 	 */
-	public static Server buildNormalServer(int port, String contextPath) {
+	public static Server createServerInSource(int port, String contextPath) throws Exception {
 		Server server = new Server(port);
 		WebAppContext webContext = new WebAppContext("src/main/webapp", contextPath);
 		webContext.setClassLoader(Thread.currentThread().getContextClassLoader());
-		webContext.setDefaultsDescriptor("src/test/resources/jetty-webdefault.xml"); // 避免windows lock,设置useFileMappedBuffer=false
+		//webContext.setDefaultsDescriptor("jetty-webdefault.xml"); // 避免windows lock,设置useFileMappedBuffer=false
+		webContext.getInitParams().put("org.eclipse.jetty.servlet.Default.useFileMappedBuffer", "false"); // 避免windows lock,设置useFileMappedBuffer=false
+		
+		//设置这个，可以加载jar的 jsp tld 文件(jsp tag file)
+		webContext.setClassLoader(new WebAppClassLoader(webContext));
+		webContext.setParentLoaderPriority(true);
+		
 		server.setHandler(webContext);
 		server.setStopAtShutdown(true);
 		return server;
