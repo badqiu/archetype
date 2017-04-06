d5645fb1789870d1f1a3464545f6caf54cb461ad
badqiu <badqiu@gmail.com>
2017-04-06 17:45:58 +0800

    1

--- web-admin/src/main/webapp/js/app_config.js
+++ web-admin/src/main/webapp/js/app_config.js
@@ -5,7 +5,7 @@ if(window.location.hostname == 'localhost') {
 	var appMode = 'prod';
 }
 
-if(appMode='dev') {
+if(appMode=='dev') {
 	var globalWsUrl="http://localhost:6060/rpc";
 }else {
 	var globalWsUrl="http://webservice.demoproject.company.com/rpc";
